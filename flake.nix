2824231a8163e45883a5ada5a24769d4ff1531d8
Jackson Breit <84806918+NinjacksonXV@users.noreply.github.com>
2025-04-22 09:29:58 -0700

    Add flake and lockfile

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            vala
+            libevdev
+            libinput # Here for tooling
+            udev
+          ];
+        };
+      };
+
+    };
+
+}
